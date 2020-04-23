e0bfaa35db510d351b85cefea2597782acb19fc0
chief141 <prashantrahul23@gmail.com>
2020-04-23 10:34:25 +0530

    Update Approximating-pi.py

--- Approximating-pi/Approximating-pi.py
+++ Approximating-pi/Approximating-pi.py
@@ -2,15 +2,15 @@ import pygame
 from random import randint
 from math import sqrt
 
-width= 400
+width = 400
 height= 400
-fps = 100
+fps   = 100
 
 #for display
 pygame.init()
-screen=pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Approximating PI")
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
 
 dots_inside_circle = 0
 total_dots = 0
@@ -24,14 +24,14 @@ actual_pi = 3.141592653589793238462643 ##ACTUAL PI JUST TO COMPARE WITH ESTIMATE
 print()
 print("Actual PI :",actual_pi)
 print()
+
 #game loop
 run=True
 last_time_print = pygame.time.get_ticks()
 while run:
 	#clock spped
 	delta_time = clock.tick(fps)/1000
-
-
+	
 	#input(events)
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
@@ -39,7 +39,6 @@ while run:
 
 	x = randint(0,r*2)
 	y = randint(0,r*2)
-
 	distance = sqrt((r - x)**2 + (r  - y)**2)
 
 	if distance < 200:
@@ -49,7 +48,6 @@ while run:
 	else:
 		total_dots += 1
 		color = (255,0,0)
-
 
 	drawing_dots_x.append(x)
 	drawing_dots_y.append(y)
@@ -64,22 +62,18 @@ while run:
 	pygame.draw.circle(screen,(255,255,255),(width//2,height//2), r)
 	pygame.draw.circle(screen,(0,0,0),(width//2,height//2), 199)
 
-
 	for i in range(len(drawing_dots_x)- 1):
 		x_ = drawing_dots_x[i]
 		y_ = drawing_dots_y[i]
 		color = drawing_dots_color[i]
-
 		screen.set_at((x_, y_), color)
 
 	if pygame.time.get_ticks() - last_time_print >= 1000:
 		PI = 4 * (dots_inside_circle/total_dots)
 		last_time_print = pygame.time.get_ticks()
 		print("Current estimated PI :",PI)
-
-	
+		
 	pygame.display.flip()
-
-
+	
 pygame.quit()
 quit()
